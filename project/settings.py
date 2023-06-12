443bcb14530dc7ef87399cae87ceceb19e9ec85a
Mazyro <amazyrko@gmail.com>
2023-06-12 04:11:01 +0300

    use Bootstrap,ht_10

--- project/settings.py
+++ project/settings.py
@@ -133,6 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static_files'
+STATICFILES_DIRS = ['assets']
 
 MEDIA_ROOT = 'media'
 MEDIA_URL = 'media/'
