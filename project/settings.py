bfb93ece5fe1d516ad9ef95cad49c4e1937518fc
Mazyro <amazyrko@gmail.com>
2023-06-18 22:19:28 +0300

    middlewares,signals, cart added

--- project/settings.py
+++ project/settings.py
@@ -67,7 +67,6 @@ MIDDLEWARE = [
     'project.middlewares.TrackingMiddleware',
     # Моя middleware для вывода стр что есть ошибка
     'project.middlewares.ErrorHandlingMiddleware',
-
 ]
 
 ROOT_URLCONF = 'project.urls'
